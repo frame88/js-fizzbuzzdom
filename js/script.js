954e7c4305cdb68ce6aaf80a27e366bb73e07ea3
frame88 <fleuzzi97@gmail.com>
2021-11-23 17:04:24 +0100

    base camp

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ if-else (i%5 == 0) print BUZZ
 if-else ((i%5 == 0 ) && (i%3 == 0)) print FIZZBUZZ
 else print i
 in tutte e 4 le condizioni modificare il colore di background e il numero (o parola)
-/*
+*/
+const container = document.querySelector('.container');
+
+for (let i = 1; i<101; i++) {
+    const stickernumb = i;
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.append(stickernumb);
+    
+    container.append(div);
+}
+
