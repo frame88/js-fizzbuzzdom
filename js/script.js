9e7a01dad606eec7f8999db47f667cf73e3ea3ea
frame88 <fleuzzi97@gmail.com>
2021-11-23 16:45:47 +0100

    skills css e schema testuale per script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+creare un for (i=0; i<=100; i++), all'interno del for:
+if (i%3 == 0) print FIZZ
+if-else (i%5 == 0) print BUZZ
+if-else ((i%5 == 0 ) && (i%3 == 0)) print FIZZBUZZ
+else print i
+in tutte e 4 le condizioni modificare il colore di background e il numero (o parola)
+/*
